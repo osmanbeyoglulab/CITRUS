ab3b4cf6d80b429eff02d0b201c598c7a9c8b285
xim2016 <xj_ma@yahoo.com>
2021-09-08 19:20:22 -0400

    Add files via upload

---
+++ ensemble_TFs.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Created By  : Xiaojun Ma
+# Created Date: Sep 7 PDT 2021
+# =============================================================================
+"""
+This script generates the ensembled TF activities from multiple CITRUS training outputs
+
+"""
+ 
+import pickle
+import pandas as pd 
+import os
+
+# input_dir = "./data"
+# output_dir = "./data"
+
+input_dir = "C:\\Users\\XIM33\\OneDrive - University of Pittsburgh\\GIT\\CITRUS_PAN_mask\\dropout_input_cpu_test_on_holdout\\output"
+output_dir = "../data"
+
+def generateTFactivity(tf, idx2can, tmr, cans, tf_name):
+    # generate the TF activity matrix
+    df_TF = pd.DataFrame(data = tf, columns = tf_name, index = tmr)
+    can_names = [idx2can[idx] for idx in cans]
+    df_TF['cancer_type'] = can_names
+    return(df_TF)
+
+#read in input dataset
+input_dir2 = "C:\\Users\\XIM33\\OneDrive - University of Pittsburgh\\GIT\\data_typeSpecific"    
+data = pickle.load( open(os.path.join(input_dir2,"dataset_PANunion2500_17_sga_dropped_seperated_rmNotImpt_0.04_with_holdout_new.pkl"), "rb"))
+
+# read in output datasets of 10 runs
+Ntf = 2
+run = list()
+for i in range(1,Ntf+1):
+    dataset = pickle.load( open(os.path.join(input_dir,"output_with_holdout_new_mask_batch100_inputdrop0.2_drop0.2_new_{}.pkl".format(i)), "rb") )
+
+    run.append(dataset) 
+
+tf = list()
+for i in range(Ntf):
+    tf.append(run[i]['hid_tmr'])
+
+## genereate ensemble tf activity matrix
+tf_ensemble = 0
+for i in range(Ntf):
+    tf_ensemble += tf[i]
+    
+tf_ensemble = tf_ensemble/Ntf
+
+df_tf = generateTFactivity(tf_ensemble, data['idx2can'],data['tmr'], data['can'], data['tf_name'])
+
+df_tf.to_csv(os.path.join(output_dir,"TF_activity_ensemble_{}.csv".format(Ntf)))
